522b781c8147123a274a969e91923b357eb88956
zohairawan <35776095+zohairawan@users.noreply.github.com>
2024-08-14 11:23:05 -0700

    Create basic swing layout

--- src/main/java/com/acupofjava/App.java
+++ src/main/java/com/acupofjava/App.java
@@ -1,7 +1,36 @@
 package com.acupofjava;
+import java.awt.*;
+import javax.swing.*;
 
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        JFrame frame = new JFrame("Test Title");
+
+        JPanel outerBox = new JPanel();
+        outerBox.setLayout(new BoxLayout(outerBox,BoxLayout.PAGE_AXIS));
+        outerBox.setBackground(Color.BLACK);
+
+        JLabel scrambledWordLabel = new JLabel("Hello1");
+        scrambledWordLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        outerBox.add(scrambledWordLabel);
+
+        JPanel innerBox = new JPanel();
+        innerBox.setLayout(new BoxLayout(innerBox, BoxLayout.LINE_AXIS));
+        innerBox.setBackground(Color.RED);
+
+        JLabel userInput = new JLabel("Hello2");
+        userInput.setAlignmentX(Component.CENTER_ALIGNMENT);
+        innerBox.add(userInput);
+        outerBox.add(innerBox);
+
+        frame.add(outerBox);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setMinimumSize(new Dimension(300, 500));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
     }
 }
