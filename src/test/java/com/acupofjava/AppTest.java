e72d2d782473e37690fd1f3d6bedc33df687d98b
Mick <cutipus@protonmail.com>
2024-08-28 21:10:43 +0300

    And this

--- src/test/java/com/acupofjava/AppTest.java
+++ src/test/java/com/acupofjava/AppTest.java
@@ -3,6 +3,24 @@ package com.acupofjava;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
+import com.acupofjava.PlayResult.*;
 
+public class AppTest {
+    public void test() {
+        PlayResult returnz = play();
+        switch (returnz) {
+            case Defeat() -> {
+            }
+            case Victory() -> {
+            }
+            case Wrong(int hpLeft) -> {
+            }
+            case Right(String nextWord) -> {
+            }
+        }
+    }
+
+    private PlayResult play() {
+        return new Defeat();
+    }
 }
