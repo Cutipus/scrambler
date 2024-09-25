1fd6991162213238443db0dd0b92319b3bbf31e9
zohairawan <35776095+zohairawan@users.noreply.github.com>
2024-09-25 11:54:17 -0700

    Add length and permutation filter

--- src/main/java/com/acupofjava/WordBundleFilter.java
+++ src/main/java/com/acupofjava/WordBundleFilter.java
@@ -25,33 +25,47 @@ public class WordBundleFilter {
         @SuppressWarnings("unchecked")
         HashMap<String, List<String>> words = (HashMap<String, List<String>>) parser.parse(reader);
 
-        HashMap<String, List<String>> filteredWord = new HashMap<>();
+        HashMap<String, List<String>> easyWords = new HashMap<>();
+        int EASY_MINIMUM_KEY_LENGTH = 3;
+        int EASY_MAXIMUM_KEY_LENGTH = 4;
+        int EASY_MINIMUM_NUMBER_OF_PERMUTATIONS = 5;
+        int EASY_MAXIMUM_NUMBER_OF_PERMUTATIONS = 8;
+
+        HashMap<String, List<String>> mediumWords = new HashMap<>();
+        int MEDIUM_MINIMUM_KEY_LENGTH = 4;
+        int MEDIUM_MAXIMUM_KEY_LENGTH = 6;
+        int MEDIUM_MINIMUM_NUMBER_OF_PERMUTATIONS = 2;
+        int MEDIUM_MAXIMUM_NUMBER_OF_PERMUTATIONS = 6;
+
+        HashMap<String, List<String>> hardWords = new HashMap<>();
+        int HARD_MINIMUM_KEY_LENGTH = 6;
+        int HARD_MAXIMUM_KEY_LENGTH = 8;
+        int HARD_MINIMUM_NUMBER_OF_PERMUTATIONS = 1;
+        int HARD_MAXIMUM_NUMBER_OF_PERMUTATIONS = 1;
+
         for (Entry<String, List<String>> permutaitonEntry : words.entrySet()) {
             String key = permutaitonEntry.getKey();
             List<String> value = permutaitonEntry.getValue();
-            if (key.length() <= 3) {
-                filteredWord.put(key, new ArrayList<>(value));
+            if (filterBasedOnLevel(permutaitonEntry, EASY_MINIMUM_KEY_LENGTH, EASY_MAXIMUM_KEY_LENGTH, EASY_MINIMUM_NUMBER_OF_PERMUTATIONS, EASY_MAXIMUM_NUMBER_OF_PERMUTATIONS)) {
+                easyWords.put(key, new ArrayList<>(value));
+            }
+
+            if (filterBasedOnLevel(permutaitonEntry, MEDIUM_MINIMUM_KEY_LENGTH, MEDIUM_MAXIMUM_KEY_LENGTH, MEDIUM_MINIMUM_NUMBER_OF_PERMUTATIONS, MEDIUM_MAXIMUM_NUMBER_OF_PERMUTATIONS)) {
+                mediumWords.put(key, new ArrayList<>(value));
+            }
+
+            if (filterBasedOnLevel(permutaitonEntry, HARD_MINIMUM_KEY_LENGTH, HARD_MAXIMUM_KEY_LENGTH, HARD_MINIMUM_NUMBER_OF_PERMUTATIONS, HARD_MAXIMUM_NUMBER_OF_PERMUTATIONS)) {
+                hardWords.put(key, new ArrayList<>(value));
             }
         }
 
+        for (Entry<String, List<String>> filteredWord : hardWords.entrySet()) {
+            System.out.println(filteredWord);
+        }
+    }
 
-        // this is how you find all permutations of a specific word:
-
-        // String unsortedWord = "telephone";
-        // char[] wordChars = unsortedWord.toCharArray();
-        // Arrays.sort(wordChars);
-        // String sortedWord = new String(wordChars);
-        // System.out.println(sortedWord);
-
-        // switch (words.get(sortedWord)) {
-        // case null -> {
-        // System.out.println("no such word");
-        // }
-
-        // case List<String> validWords -> {
-        // System.out.println("these are the words: " + String.join(", ", validWords));
-        // }
-        // }
-
+    private static boolean filterBasedOnLevel(Entry<String, List<String>> entry, int minKeyLength, int maxKeyLength, int minPermutation, int maxPermutation) {
+        return entry.getKey().length() >= minKeyLength && entry.getKey().length() <= maxKeyLength
+                && entry.getValue().size() >= minPermutation && entry.getValue().size() <= maxPermutation;
     }
 }
