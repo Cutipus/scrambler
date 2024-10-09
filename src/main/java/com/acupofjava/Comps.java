87b596e761b29082ba8b56bb60bea7043c3b4dbc
Mick <cutipus@protonmail.com>
2024-10-09 23:02:56 +0300

    Move GradiantButton to Comps

--- src/main/java/com/acupofjava/Comps.java
+++ src/main/java/com/acupofjava/Comps.java
@@ -92,3 +92,30 @@ public class Comps {
         return box;
     }
 }
+
+class GradiantButton extends JButton {
+    // TODO: move to comps
+    private Color topColor;
+    private Color bottomColor;
+
+    GradiantButton(Color topColor, Color bottomColor, String text) {
+        super(text);
+        setContentAreaFilled(false);
+        this.topColor = topColor;
+        this.bottomColor = bottomColor;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        final Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setPaint(new GradientPaint(
+                new Point(0, 0),
+                topColor,
+                new Point(0, getHeight()),
+                bottomColor));
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+        g2d.dispose();
+
+        super.paintComponent(g);
+    }
+}
