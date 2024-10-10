e9daed86cb8a562a3658e84c3facb5c886e662b7
zohairawan <35776095+zohairawan@users.noreply.github.com>
2024-10-10 11:16:24 -0700

    Move word list into main as local variable

--- src/main/java/com/acupofjava/App.java
+++ src/main/java/com/acupofjava/App.java
@@ -6,13 +6,12 @@ import java.util.Set;
 public class App {
     private static final int STARTING_HP = 3;
 
-    // TODO: use selected_words.json
-    public static final Map<String, Set<String>> words = Map.of(
-            "eilv", Set.of("evil", "levi", "live", "veil", "vile", "vlei"),
-            "ehllo", Set.of("hello"),
-            "oww", Set.of("wow"));
-
     public static void main(String[] args) {
+        // TODO: use selected_words.json
+        final Map<String, Set<String>> words = Map.of(
+                "eilv", Set.of("evil", "levi", "live", "veil", "vile", "vlei"),
+                "ehllo", Set.of("hello"),
+                "oww", Set.of("wow"));
         Game game = new Game(words, STARTING_HP);
         UILogic ui = new UILogic(game);
 
